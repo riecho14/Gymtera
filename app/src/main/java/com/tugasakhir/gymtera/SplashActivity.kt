1b7c56921b817bdbcb6a1c5260b1d10d6a4498fd
riecho14 <riecho14@outlook.com>
2023-10-27 21:27:07 +0700

    Style(SplashActivity): Create logo and splashscreen

---
+++ app/src/main/java/com/tugasakhir/gymtera/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.tugasakhir.gymtera
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.tugasakhir.gymtera.databinding.ActivitySplashBinding
+
+@Suppress("DEPRECATION")
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val i = Intent(
+                this@SplashActivity,
+                MainActivity::class.java
+            )
+            startActivity(i)
+            finish()
+        }, 2000)
+    }
+}
