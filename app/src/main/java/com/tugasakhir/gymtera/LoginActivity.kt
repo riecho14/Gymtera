0d7becc3b7998a41fd53fb8487ab475bb6a73d52
riecho14 <riecho14@outlook.com>
2023-10-29 15:42:53 +0700

    Feat(LoginActivity, RegisterActivity): Added textwatcher feature for user input

--- app/src/main/java/com/tugasakhir/gymtera/LoginActivity.kt
+++ app/src/main/java/com/tugasakhir/gymtera/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.tugasakhir.gymtera
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.tugasakhir.gymtera.databinding.ActivityLoginBinding
 
@@ -13,11 +16,52 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Text Watcher
+        binding.textEmail.editText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.textEmail.isErrorEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val email = s.toString()
+                if (!isEmailValid(email)) {
+                    binding.textEmail.error = getString(R.string.email_error)
+                    binding.textEmail.isErrorEnabled = true
+                }
+            }
+        })
+
+        binding.textPassword.editText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.textPassword.isErrorEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val password = s.toString()
+                if (password.length < 8) {
+                    binding.textPassword.error = getString(R.string.password_error)
+                    binding.textPassword.isErrorEnabled = true
+                }
+            }
+        })
+
         // Click Listener
         binding.bottom2.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
