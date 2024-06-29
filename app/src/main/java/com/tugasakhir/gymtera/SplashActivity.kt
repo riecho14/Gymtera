114d631d09be681e7703251dc19d59b3acd3c7a2
Ericho Aldo Firano <riecho14@outlook.com>
2024-06-29 19:54:21 +0700

    Refactor(SplashActivity): UI transitions using jetpack compose

--- app/src/main/java/com/tugasakhir/gymtera/SplashActivity.kt
+++ app/src/main/java/com/tugasakhir/gymtera/SplashActivity.kt
@@ -2,32 +2,86 @@ package com.tugasakhir.gymtera
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
-import com.tugasakhir.gymtera.databinding.ActivitySplashBinding
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tugasakhir.gymtera.ui.theme.GymteraTheme
 import com.tugasakhir.gymtera.user.LoginActivity
+import kotlinx.coroutines.delay
 
 @Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
-
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        enableEdgeToEdge()
+        setContent {
+            GymteraTheme {
+                SplashScreen {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+            }
+        }
+    }
+}
 
-        Handler().postDelayed({
-            val i = Intent(
-                this@SplashActivity, LoginActivity::class.java
-            )
-            startActivity(i)
-            finish()
-        }, 2000)
+val CustomFont = FontFamily(Font(R.font.ft_semibold))
+
+@Composable
+fun SplashScreen(onTimeout: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(132.dp)
+        )
+        Text(
+            text = "GYMTERA",
+            color = Color.Black,
+            fontFamily = CustomFont,
+            fontSize = 26.sp,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(all = 62.dp)
+        )
+    }
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        onTimeout()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun SplashScreenPreview() {
+    GymteraTheme {
+        SplashScreen {}
     }
 }
